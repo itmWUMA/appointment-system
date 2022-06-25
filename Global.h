d1b8a5de94f1e036d3a008121d003a7227243334
itmWuma <2913850205@qq.com>
2022-06-25 11:45:44 +0800

    添加资源文件

--- Global.h
+++ Global.h
@@ -4,12 +4,12 @@
 */
 
 // 管理员文件
-#define ADMIN_FILE "admin.dat"
+#define ADMIN_FILE "./Data/admin.dat"
 // 学生文件
-#define STU_FILE "student.dat"
+#define STU_FILE "./Data/student.dat"
 // 教师文件
-#define TEACHER_FILE "teacher.dat"
+#define TEACHER_FILE "./Data/teacher.dat"
 // 机房文件
-#define ROOM_FILE "room.dat"
+#define ROOM_FILE "./Data/room.dat"
 // 预约信息文件
-#define ORDER_FILE "order.dat"
+#define ORDER_FILE "./Data/order.dat"
